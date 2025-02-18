57d9350414bc86cea413d1a28c1d3ae4dfdc5869
BernCell <bern.cell@hotmail.com>
2025-02-18 23:43:11 +0100

    1er commit cours-redux

---
+++ src/reducers/user.reducer.js
@@ -0,0 +1,9 @@
+const initialState = { user: "Bern" };
+
+export default function userReducer(state = initialState, action) {
+    //SWITCH
+    // switch (action.type) {
+    //     default:
+    // }
+    return state
+}
