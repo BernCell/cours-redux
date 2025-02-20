1068e0391320032034e4a08f7e1d75114f5fa9f4
BernCell <bern.cell@hotmail.com>
2025-02-20 23:46:23 +0100

    6eme commit user likes

--- src/actions/user.action.js
+++ src/actions/user.action.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const GET_USER = "GET_USER"
+export const ADD_USER_LIKE = "ADD_USER_LIKE"
 
 export const getUser = () => {
     return (dispatch) => {
@@ -9,6 +10,15 @@ export const getUser = () => {
 
             dispatch({ type: GET_USER, payload: res.data[0] })
 
-        })
-    }
-}
+        });
+    };
+};
+
+export const addUserLike = (data) => {
+    return (dispatch) => {
+        return axios.put(`http://localhost:3000/user/${data.id}`, data).then((res) => {
+            dispatch({ type: ADD_USER_LIKE, payload: data })
+
+        });
+    };
+};
--- src/components/Like.js
+++ src/components/Like.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addPostLike } from "../actions/post.action";
+import { addUserLike } from "../actions/user.action";
 
 const Like = ({ post }) => {
 
   const dispatch = useDispatch();
-
+  const user = useSelector((state) => state.userReducer)
   const handleLike = () => {
 
     const postData = {
@@ -15,8 +16,15 @@ const Like = ({ post }) => {
       id: post.id,
       likes: post.likes + 1,
     };
+    const userData = {
+      pseudo: user.pseudo,
+      likes: user.likes + 1,
+      age: user.age,
+      id: user.id,
+    };
 
     dispatch(addPostLike(postData));
+    dispatch(addUserLike(userData))
   };
 
   return (
